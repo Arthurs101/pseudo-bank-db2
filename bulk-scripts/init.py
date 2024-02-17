e69bfcd71c47401bc8faff4fed36d557bddad429
Arthurs101 <84546401+Arthurs101@users.noreply.github.com>
2024-02-17 15:03:07 -0600

    fixed bulk operation script

--- bulk-scripts/init.py
+++ bulk-scripts/init.py
@@ -1,11 +1,7 @@
 import random
 from faker import Faker
-from datetime import datetime
 
 fake = Faker()
-
-# Lista de tipos de usuario
-user_types = ['client', 'admin', 'personnel']
 
 # Lista de tipos de cuentas
 account_types = ['monetary', 'savings', 'business']
@@ -36,14 +32,15 @@ def generate_user():
         "phones": [{
             "number": fake.random_number(digits=8),
             "postal_code": random.choice(['+502', '+1']),
-            "brand": random.choice(['tigo', 'claro'])
+            "brand": random.choice(['tigo', 'claro','at&t'])
         }],
         "adrresses": [{
             "street_name": fake.street_name(),
             "zip_code": fake.zipcode(),
             "city": fake.city()
         }],
-        "loans": []
+        "loans": [],
+        "type":"client"
     }
     
     # Generar al menos una cuenta bancaria
@@ -53,8 +50,7 @@ def generate_user():
         "balance": round(random.uniform(100, 10000), 2),
         "currency": random.choice(currencies),
         "status": random.choice(account_statuses),
-        "created_at": fake.date_this_century().strftime('%d/%m/%Y'),
-        "created_in": fake.random_number(digits=8)
+        "created_at": fake.date_this_century().strftime('%d/%m/%Y')
     }
     user["accounts"].append(account)
     
@@ -67,24 +63,9 @@ def generate_user():
             "balance": round(random.uniform(100, 10000), 2),
             "currency": random.choice(currencies),
             "status": random.choice(account_statuses),
-            "created_at": fake.date_this_century().strftime('%d/%m/%Y'),
-            "created_in": fake.random_number(digits=8)
+            "created_at": fake.date_this_century().strftime('%d/%m/%Y')
         }
         user["accounts"].append(account)
-    
-    # Generar préstamos
-    num_loans = random.randint(0, 2)
-    for _ in range(num_loans):
-        loan = {
-            "ammount": round(random.uniform(1000, 50000), 2),
-            "due_date": fake.future_date(end_date='+5y').strftime('%d/%m/%Y'),
-            "currency": random.choice(currencies),
-            "payments": [],
-            "status": random.choice(['payed', 'due', 'active']),
-            "interest": round(random.uniform(0, 1), 2),
-            "interest_rate": fake.word()
-        }
-        user["loans"].append(loan)
     
     return user
 
@@ -99,44 +80,8 @@ def generate_branch():
     }
     return branch
 
-# Función para generar transacciones
-def generate_transaction(users):
-    transaction = {
-        "ammount": round(random.uniform(1, 10000), 2),
-        "date": fake.date_this_year().strftime('%d/%m/%Y'),
-        "currency": random.choice(currencies),
-        "from": random.choice(users)['_id'],
-        "account_from": random.choice(random.choice(users)['accounts'])['_id'],
-        "to": random.choice(users)['_id'],
-        "account_to": random.choice(random.choice(users)['accounts'])['_id']
-    }
-    return transaction
-
-# Generar usuarios admin y personnel
-admin_count = 0
-personnel_count = 0
-admins = []
-personnel = []
-while admin_count + personnel_count < 1000:
-    user = generate_user()
-    if user['type'] == 'admin' and admin_count < 100:
-        admins.append(user)
-        admin_count += 1
-    elif user['type'] == 'personnel':
-        personnel.append(user)
-        personnel_count += 1
-
-# Generar usuarios restantes
-remaining_users = [generate_user() for _ in range(1000 - (admin_count + personnel_count))]
-
-# Combinar todos los usuarios
-users = admins + personnel + remaining_users
-
 # Generar sucursales
-branches = [generate_branch() for _ in range(100)]
-
-# Generar transacciones
-transactions = [generate_transaction(users) for _ in range(10000)]
+branches = [generate_branch() for _ in range(200)]
 
 # Guardar en MongoDB (requiere tener MongoDB instalado y corriendo)
 from pymongo import MongoClient
@@ -144,13 +89,35 @@ from pymongo import MongoClient
 client = MongoClient('localhost', 27017)
 db = client['bank_database']
 
-# Insertar datos en colecciones
-users_collection = db['users']
+# Insertar datos de sucursales en la colección de sucursales
 branches_collection = db['branches']
-transactions_collection = db['transactions']
-
-users_collection.insert_many(users)
 branches_collection.insert_many(branches)
-transactions_collection.insert_many(transactions)
+
+# Recuperar los _id de las sucursales
+branch_ids = [branch['_id'] for branch in branches]
+
+# Generar usuarios con cuentas bancarias
+users = [generate_user() for _ in range(50000)]
+
+
+#General empleados y admins
+for _ in range(200):
+    user = generate_user()
+    user["type"] = "admin"
+    users.append(user)
+
+for _ in range(800):
+    user = generate_user()
+    user["type"] = "personnel"
+    users.append(user)
+
+# Asignar aleatoriamente un _id de sucursal a cada cuenta bancaria generada
+for user in users:
+    for account in user["accounts"]:
+        account["created_in"] = random.choice(branch_ids)
+
+# Insertar datos de usuarios en la colección de usuarios
+users_collection = db['users']
+users_collection.insert_many(users)
 
 print("Datos insertados exitosamente en la base de datos MongoDB.")
