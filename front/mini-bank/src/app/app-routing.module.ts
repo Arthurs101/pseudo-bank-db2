0697f363e33e1a73ec52bd3a3e8a2c0505d11759
AGM54 <134453750+AGM54@users.noreply.github.com>
2024-02-24 05:59:19 -0600

    cambios para el dash
    dash

--- front/mini-bank/src/app/app-routing.module.ts
+++ front/mini-bank/src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './login/login.component'; // Asegúrate de importar LoginComponent
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' }, // Redirige a login por defecto
+  // otras rutas...
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
