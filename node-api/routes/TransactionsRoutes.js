a6081d2e27f5af50e654d36b0d6a1ae486e65a2a
Arthurs101 <84546401+Arthurs101@users.noreply.github.com>
2024-02-25 08:59:28 -0600

    not implemented loans yet

--- node-api/routes/TransactionsRoutes.js
+++ node-api/routes/TransactionsRoutes.js
@@ -2,5 +2,7 @@ const {Router} = require('express');
 const router = Router();
 const transactionController = require('../controllers/TransactionsController')
 
-router.post('/new',transactionController.newTransaction);
+router.post('/transaction',transactionController.newTransaction);
+router.post('/loans')
+router.put('/loans')
 module.exports = router
