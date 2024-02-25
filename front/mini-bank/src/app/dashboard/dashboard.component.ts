1c6ad67176d47c19a7e4d84908075ad24663dded
AGM54 <134453750+AGM54@users.noreply.github.com>
2024-02-25 01:07:47 -0600

    gráfico
    gráfico, cambio de botones

--- front/mini-bank/src/app/dashboard/dashboard.component.ts
+++ front/mini-bank/src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Chart, ChartConfiguration, registerables } from 'chart.js';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,12 +7,50 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
+  saldoUsuario: number = 1000; 
+  numeroCuenta: string = '123456789'; 
+  chart: Chart | undefined; 
 
-  saldoUsuario: number = 100; 
-  numeroCuenta: number=100000;
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor() {
+  
+    Chart.register(...registerables);
   }
 
+  ngOnInit(): void {
+    this.initChart();
+  }
+
+  initChart(): void {
+    const config: ChartConfiguration = {
+      type: 'doughnut', 
+      data: {
+        labels: ['Depósitos', 'Débitos', 'Retiros', 'Compras'],
+        datasets: [{
+          label: 'Transacciones del último mes',
+          data: [300, 500, 100, 200], 
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)'
+          ],
+          borderColor: [
+            'rgba(255,99,132,1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)'
+          ],
+          borderWidth: 1
+        }]
+      },
+      options: {
+      
+      }
+    };
+
+    const ctx = document.getElementById('myChart') as HTMLCanvasElement;
+    if (ctx) {
+      this.chart = new Chart(ctx, config);
+    }
+  }
 }
