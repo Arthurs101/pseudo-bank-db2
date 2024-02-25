49cd4ec8c77b599dcfde97705c3df004357ea8c9
AGM54 <134453750+AGM54@users.noreply.github.com>
2024-02-25 04:49:32 -0600

    c
    deje comentando donde va la lógica de las transacciones

--- front/mini-bank/src/app/historial-transacciones/historial-transacciones.component.ts
+++ front/mini-bank/src/app/historial-transacciones/historial-transacciones.component.ts
@@ -8,3 +8,32 @@ import { Component } from '@angular/core';
 export class HistorialTransaccionesComponent {
 
 }
+
+//dejo comentando esto q es para importar las transacciones  y mostrarlas
+
+// import { Component, OnInit } from '@angular/core';
+
+// import { TransactionService } from '../transaction.service'; 
+// import { Transaction } from '../transaction.model'; 
+
+// @Component({
+//   selector: 'app-historial-transacciones',
+//   templateUrl: './historial-transacciones.component.html',
+//   styleUrls: ['./historial-transacciones.component.scss']
+// })
+// export class HistorialTransaccionesComponent implements OnInit {
+//   transacciones: Transaction[] = [];
+
+//   constructor(private transactionService: TransactionService) {}
+
+//   ngOnInit() {
+//     this.transactionService.getTransactions().subscribe(
+//       (transacciones) => {
+//         this.transacciones = transacciones;
+//       },
+//       (error) => {
+//         console.error('Error al obtener transacciones', error);
+//       }
+//     );
+//   }
+// }
