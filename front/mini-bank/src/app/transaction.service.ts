b5dc964beb41f37a7819fdf2de34ceab6842f701
AGM54 <134453750+AGM54@users.noreply.github.com>
2024-02-25 04:33:25 -0600

    Create transaction.service.ts

---
+++ front/mini-bank/src/app/transaction.service.ts
@@ -0,0 +1,18 @@
+
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Transaction } from './transaction.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TransactionService {
+  private apiUrl = 'tu_api_url'; // api url
+
+  constructor(private http: HttpClient) {}
+
+  getTransactions(): Observable<Transaction[]> {
+    return this.http.get<Transaction[]>(this.apiUrl);
+  }
+}
