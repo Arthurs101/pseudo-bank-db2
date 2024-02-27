83231a764f48f0e96f112f746c817276484d6228
AGM54 <134453750+AGM54@users.noreply.github.com>
2024-02-27 05:37:04 -0600

    usuario datos
    Para poner los datos del usuario y que los vea

--- front/mini-bank/src/app/user-settings/user-settings.component.ts
+++ front/mini-bank/src/app/user-settings/user-settings.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { minibankService } from '../backend.service';
+import { User } from '../usermodels.model';
 
 @Component({
   selector: 'app-user-settings',
   templateUrl: './user-settings.component.html',
   styleUrls: ['./user-settings.component.scss']
 })
-export class UserSettingsComponent {
+export class UserSettingsComponent implements OnInit {
+  user: User | null = null;
 
+  constructor(private service: minibankService) {}
+
+  ngOnInit(): void {
+    this.user = this.service.getUser();
+  }
 }
