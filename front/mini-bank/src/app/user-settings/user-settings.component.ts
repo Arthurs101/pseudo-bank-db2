d0f408c39c57b5c0f5e74ef502e2a91c793af04e
AGM54 <134453750+AGM54@users.noreply.github.com>
2024-02-27 18:07:51 -0600

    cam
    cam

--- front/mini-bank/src/app/user-settings/user-settings.component.ts
+++ front/mini-bank/src/app/user-settings/user-settings.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { minibankService } from '../backend.service';
 import { User } from '../usermodels.model';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-user-settings',
@@ -9,10 +10,45 @@ import { User } from '../usermodels.model';
 })
 export class UserSettingsComponent implements OnInit {
   user: User | null = null;
+  userForm: FormGroup;
 
-  constructor(private service: minibankService) {}
+  constructor(private service: minibankService, private formBuilder: FormBuilder) {
+
+    this.userForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      lastnames: ['', Validators.required], 
+      nationality: ['', Validators.required], // Añadido
+
+    });
+  }
 
   ngOnInit(): void {
     this.user = this.service.getUser();
+    if (this.user) {
+    
+      this.userForm.patchValue({
+        name: this.user.names,
+        lastnames: this.user.lastnames, 
+        nationality: this.user.nationality,
+       
+      });
+    }
+  }
+
+  onSubmit(): void {
+    if (this.userForm.valid) {
+  
+      this.service.updateUser(this.userForm.value).subscribe({
+        next: updatedUser => {
+    
+          this.service.setUser(updatedUser);
+          alert('Usuario actualizado con éxito.');
+        },
+        error: error => {
+         console.log(error)
+         alert('Error al actualizar el usuario. Por favor, inténtelo de nuevo.')
+        }
+      });
+    }
   }
 }
